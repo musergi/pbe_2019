4fa4bdc0272271be3d5da416a6360b16fc5c4d2e
Sergi Mus <sergi.mus@gmail.com>
2019-09-26 13:03:12 +0200

    Hotfix and added .gitignore

--- rfid/rfid.py
+++ rfid/rfid.py
@@ -1,22 +1,20 @@
 from py532lib.i2c import Pn532_i2c
 
 class RfidReader:
-    def __init__(self):
-        pass
 
     def read_uid(self):
-        return ""
+        raise Exception("Methode not overriten in subclass")
 
-class RfidReaderI2C(RfidReader):
+class RfidReader_pn532_i2c(RfidReader):
     def __init__(self):
         self.device = Pn532_i2c()
         self.device.SAMconfigure()
 
     def read_uid(self):
         card_data = self.device.read_mifare().get_data()
-        return ''.join('{:02x}'.format(x) for x in card_data[-4:])
+        return ''.join('{:02x}'.format(byte) for byte in card_data[-4:])
 
 if __name__ == '__main__':
-    rfid = RfidReaderI2C()
+    rfid = RfidReader_pn532_i2c()
     uid = rfid.read_uid()
     print(uid)
