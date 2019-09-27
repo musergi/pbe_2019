9ae6f55c5cecf1d06548f4560ca6613126837b4e
Sergi Mus <sergi.mus@gmail.com>
2019-09-27 18:41:44 +0200

    Updated interface

--- rfid/interface.py
+++ rfid/interface.py
@@ -1,8 +1,22 @@
 import gi
-gi.require_version("Gtk", "3.0")
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-window = Gtk.Window(title="Hello World")
-window.show()
-window.connect("destroy", Gtk.main_quit)
-Gtk.main()
+WINDOW_CAPTION = 'Rfid app'
+
+class Window(Gtk.Window):
+    def __init__(self):
+        super().__init__(self, title=WINDOW_CAPTION)
+        self.connect("destroy", Gtk.main_quit)
+
+        self.button = Gtk.Button(label='Click here')
+        self.button .connect('clicked', self.button_action)
+        self.add(self.button)
+
+    def button_action(self, widget):
+        print('Button pressed')
+
+if __name__ == '__main__':
+    window = Window(title="Hello World")
+    window.show()
+    Gtk.main()
