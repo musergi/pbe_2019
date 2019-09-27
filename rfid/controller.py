00eba958c9ff27021b7298a903d9fde1bdbdaae2
Sergi Mus <sergi.mus@gmail.com>
2019-09-27 19:53:35 +0200

    Added controller functionality

--- rfid/controller.py
+++ rfid/controller.py
@@ -1,11 +1,33 @@
+# System imports
+import threading
+
+# Module imports
 from .model import RfidReader_pn532_i2c
+from .view import WAITING_UID_TEXT, READ_UID_TEXT
+
 
 class Controller:
-    def __init__(self):
-        self.rfid_reader = RfidReader_pn532_i2c()
+    def __init__(self, window):
+        self._window = window
+
+        self._rfid_reader = RfidReader_pn532_i2c()
+        self._lock = threading.Lock()
+
+        # Start uid waiter
+        self._uid_waiter = threading.Thread(target=set_uid, daemon=True)
 
     def clear_uid(self):
-        pass
+        print('[Controller] - Clearing uid')
+        with self._lock:
+            self._window.set_label(WAITING_UID_TEXT)
 
     def set_uid(self):
-        pass
+        print('[Controller] - Setting uid')
+        uid = self._rfid_reader.read_uid()
+        with self._lock:
+            self._window.set_label(READ_UID_TEXT.format(uid))
+
+        # Create another waiter before closing previous
+        self._uid_waiter = threading.Thread(target=set_uid, daemon=True)
+
+
--- rfid/view.py
+++ rfid/view.py
@@ -1,30 +1,43 @@
+# Third party imports
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+# Module imports
+from .controller import Controller
+
+# Constants
 WINDOW_CAPTION = 'Rfid app'
 
 CLEAR_BUTTON_TEXT = 'Click to clear'
 WAITING_UID_TEXT = 'Insert card for uid display'
 READ_UID_TEXT = 'Read card with uid {}'
 
+
 class Window(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title=WINDOW_CAPTION)
         self.connect("destroy", Gtk.main_quit)
 
+        # Create grid layout
         self.grid_layout = Gtk.Grid()
         self.add(self.grid_layout)
 
+        # Add label
         self.label = Gtk.Label(label=WAITING_UID_TEXT)
         self.grid_layout.attach(self.label, 0, 0, 1, 1)
 
+        # Add button
         self.button = Gtk.Button(label=CLEAR_BUTTON_TEXT)
-        self.button.connect('clicked', self.button_action)
         self.grid_layout.attach(self.button, 0, 1, 1, 1)
 
-    def button_action(self, widget):
-        print('Button pressed')
+        # Set-up controller
+        self.ctl = Controller(self)
+        self.button.connect('clicked', self.ctl.clear_uid)
+
+    def set_label(self, text):
+        self.label.set_label(text)
+
 
 if __name__ == '__main__':
     window = Window()
