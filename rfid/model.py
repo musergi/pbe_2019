7a27cf58be346af76678bd4d51d276c646a6e330
Sergi Mus <sergi.mus@gmail.com>
2019-10-02 15:43:42 +0200

    Hotfix

--- rfid/model.py
+++ rfid/model.py
@@ -1,18 +1,13 @@
 from py532lib.i2c import Pn532_i2c
 
-class RfidReader:
-
-    def read_uid(self):
-        raise Exception("Methode not overriten in subclass")
-
-class RfidReader_pn532_i2c(RfidReader):
+class RfidReader_pn532_i2c:
     def __init__(self):
         self.device = Pn532_i2c()
         self.device.SAMconfigure()
 
     def read_uid(self):
         card_data = self.device.read_mifare().get_data()
-        return ''.join('{:02x}'.format(byte) for byte in card_data[-4:])
+        return ''.join('{:02X}'.format(byte) for byte in card_data[-4:])
 
 if __name__ == '__main__':
     rfid = RfidReader_pn532_i2c()
