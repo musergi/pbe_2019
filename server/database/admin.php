0f6e1b85a1c38c7d453c1afd1bcda85daeb94462
Sergi Mus <sergi.mus@gmail.com>
2019-10-29 10:25:23 +0100

    Created admin system

---
+++ server/database/admin.php
@@ -0,0 +1,16 @@
+<h3>Add User</h3>
+<form action="commands/add_user.php">
+  UID:
+  <input type="text" name="uid">
+  <br>
+  Name:
+  <input type="text" name="name">
+  <br>
+  Last name:
+  <input type="text" name="last name">
+  <br>
+  DNI:
+  <input type="text" name="dni">
+  <br>
+  <input type="submit" value="Add user">
+</form>
