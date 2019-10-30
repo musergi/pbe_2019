f62cb1878af4d89dfa8fcab7a66b8712dbde0ea5
root <root@LAPTOP-2O89JDVM.localdomain>
2019-10-30 15:11:00 +0100

    Connect to server without counting

--- server/python/connect_server.php
+++ server/python/connect_server.php
@@ -1,20 +1,26 @@
 <?php
 //numer of times entered the server
-$count_nteserver;
+$count_nteserver ;
 
 class server{
 
     function server(){
-        $GLOBALS['count_nteserver'] = $GLOBALS['count_nteserver'] + 1;
+        if ($GLOBALS['count_nteserver'] == 0){
+            $GLOBALS['count_nteserver'] = 1;
+            echo "hola  = 0 <br>";
+            echo $count_nteserver."<br>";
+        }else{
+            $GLOBALS['count_nteserver']++;
+            echo "hola  = mas <br>";
+        }
+
+        //$GLOBALS['count_nteserver'] = $GLOBALS['count_nteserver'] + 1;
         echo "You entered the server succesfully!<br>";
         echo "Number of times entered: ".$GLOBALS['count_nteserver'];
     }
 
-    function enter_server(){
-        $GLOBALS['count_nteserver']++;
-        echo "You entered the server succesfully!";
-        echo "Number of times entered: ".$GLOBALS['count_nteserver'];
-    }
+    /*function enter_server(){
+    }*/
 
 }
 
