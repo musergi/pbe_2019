4f4e54958e1a2176c34eaae8f1e45697f152876c
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2019-11-06 21:21:31 +0100

    coded get_student and get_query

--- client/coms/core.py
+++ client/coms/core.py
@@ -1,12 +1,13 @@
 # Default Python imports
 import urllib.request # Encapsulates communication functions
 
+#Useful information: https://docs.python.org/3.1/howto/urllib2.html
 
 class CommunicationManager:
-    """
-    Given a URL, connect to that URL and extract the data from the package
-    """
+    
     def get(self, url):
+        """ Given a URL, connect to that URL and extract the data from the package 
+        Returns (String): the data in utf-8"""
         response = urllib.request.urlopen(url)
         data = response.read()
         return data.decode("utf-8")
@@ -16,11 +17,20 @@ class CommunicationManager:
         If the response is 404 return None otherwise return a Student object
         with the id, name, surname and uid stored in it.
         Returns (Student): Student created from the info provided by the server."""
-        raise Exception('Not implemented')
+        request = urllib.request.Request('uid')
+        student = urllib.request.urlopen(request)
+        if (student.get_header() == "Error404"):
+            return None
+        return student.read()
 
     def get_query(self, student, table):
-        """Get request with the student id for authentication"""
-        raise Exception('Not implemented')
+        """Get request with the student id for authentication
+        Send table name and save the information 
+        Returns (table): table is a .csv with all the table's info provided by the server"""
+        data = urllib.parse.urlencode(table)
+        request = urllib.request.Request(student.get_uid(), data)
+        self.table = urllib.request.urlopen(request)
+        return table.read()
 
 
 class Student:
@@ -32,6 +42,17 @@ class Student:
         self._uid = uid
 
     #TODO: Getters
+    def get_id(self):
+        return self._id
+    
+    def get_name(self):
+        return self._name
+    
+    def get_surname(self):
+        return self._surname
+
+    def get_uid(self):
+        return self._uid
 
     def __str__(self):
         """String representation of the class."""
