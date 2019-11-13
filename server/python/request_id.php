e95cd594b3b19d54dba794dba4a737affc137a55
root <root@LAPTOP-2O89JDVM.localdomain>
2019-11-13 16:53:18 +0100

    correcion de typos y fallo en el switch case

--- server/python/request_id.php
+++ server/python/request_id.php
@@ -4,13 +4,13 @@ require_once '../database/database.php';
 $connection = access_database();
 
 //Get the uid from the database
-$sql = 'SELECT id, name, lastname, uid FROM students WHERE uid="' . $_GET['uid'] . '";';
+$sql = 'SELECT id, name, surname, uid FROM students WHERE uid="' . $_GET['uid'] . '";';
 $result = mysqli_query($connection, $sql);
 
 //If exits return al the data, else ERROR
 if(mysqli_num_rows($result) > 0){
-    echo "id,name,lastename,uid<br>";
-    echo $row["id"].",".$row["name"].",".$row[lastname].",".$row["uid"]."<br>";
+    echo "id,name,surname,uid<br>";
+    echo $row["id"].",".$row["name"].",".$row["surname"].",".$row["uid"]."<br>";
 }else{
     header("HHTP/1.0 404 Not Found");
 }
--- server/python/request_table.php
+++ server/python/request_table.php
@@ -6,24 +6,24 @@ $connection = access_database();
 $table_name = $_GET("table_name");
 
 switch($table_name){
-    case tasks :
-        $sql = 'SELECT date, subject, name FROM tasks';
+    case 'tasks' :
+        $sql = 'SELECT date, subject, name FROM tasks;';
         $result = mysqli_query($connection, $sql);
         if(mysqli_num_rows($result)>0){
             echo "date,subject,name<br>";
-            while($row = $result->getch_assoc()){
+            while($row = $result->fetch_assoc()){
                 echo $row["date"].",".$row["subject"].",".$row["name"]."<br>";
             }
         }else{
             header("HHTP/1.0 404 Not Found");
         }
         break;
-    case marks :
+    case 'marks' :
         $sql = 'SELECT subject, name, mark FROM marks WHERE student="' .$_GET['id'].'";';
         $result = mysqli_query($connection, $sql);
         if(mysqli_num_rows($result)>0){
             echo "subject,name,mark<br>";
-            while($row = $result->getch_assoc()){
+            while($row = $result->fetch_assoc()){
                 echo $row["subject"].",".$row["name"].",".$row["mark"]."<br>";
             }
         }else{
@@ -31,11 +31,11 @@ switch($table_name){
         }
         break;
     case timetables :
-        $sql = 'SELECT day, hour, subject, room FROM timetables';
+        $sql = 'SELECT day, hour, subject, room FROM timetables;';
         $result = mysqli_query($connection, $sql);
         if(mysqli_num_rows($result)>0){
             echo "day,hour,subject,room<br>";
-            while($row = $result->getch_assoc()){
+            while($row = $result->fetch_assoc()){
                 echo $row["day"].",".$row["hour"].",".$row["subject"].",".$row["room"]."<br>";
             }
         }else{
