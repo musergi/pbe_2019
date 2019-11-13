0dd10c239204cc7614bccf2a8dede52beb14b346
Norma Gutiérrez <normagutierrez@Normas-MacBook-Air.local>
2019-11-13 16:53:40 +0100

    Upgraded comms and rfid

--- client/coms/core.py
+++ client/coms/core.py
@@ -1,10 +1,13 @@
 # Default Python imports
 import urllib.request # Encapsulates communication functions
+import csv
+from io import StringIO
 
 #Useful information: https://docs.python.org/3.1/howto/urllib2.html
+url ='http://10.0.30.203:8000/python/' 
 
 class CommunicationManager:
-    
+
     def get(self, url):
         """ Given a URL, connect to that URL and extract the data from the package 
         Returns (String): the data in utf-8"""
@@ -17,21 +20,37 @@ class CommunicationManager:
         If the response is 404 return None otherwise return a Student object
         with the id, name, surname and uid stored in it.
         Returns (Student): Student created from the info provided by the server."""
-        request = urllib.request.Request('uid')
-        student = urllib.request.urlopen(request)
-        if (student.get_header() == "Error404"):
+        values = {
+            'uid' : uid }
+
+        url_student = url + 'request_id.php'
+        data = urllib.parse.urlencode(values)
+        request = urllib.request.Request(url_student, data)
+        response = urllib.request.urlopen(request)
+        if (response.get_header() == "Error404"):
             return None
-        return student.read()
+        response_csv = response.read().decode()
+        readCSV = csv.DictReader(StringIO(response_csv), delimiter=',')
+
+        student_info = next(readCSV)
+        student = Student(student_info['card_uid'], student_info['name'], student_info['surname'], student_info['id'])
+        return student
+        
 
     def get_query(self, student, table_name):
         """Get request with the student id for authentication
         Send table name and save the information 
         Returns (table): table is a .csv with all the table's info provided by the server"""
-        data = urllib.parse.urlencode(table_name)
-        request = urllib.request.Request(student.get_uid(), data)
+        values = {
+            'student_id' : student.get_id(),
+            'table_name' : table_name }
+
+        url_query = url + 'request_table.php'
+        data = urllib.parse.urlencode(values)
+        request = urllib.request.Request(url_query, data)
         table = urllib.request.urlopen(request)
         return table.read()
-
+       
 
 class Student:
     """Class representing a student"""
