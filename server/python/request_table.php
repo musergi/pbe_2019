ac215f4c1cf65f3ac840a3b5450c8747a31486c7
Sergi Mus <sergi.mus@gmail.com>
2019-11-17 20:19:01 +0100

    Hotfix

--- server/python/request_table.php
+++ server/python/request_table.php
@@ -3,16 +3,16 @@
 require_once '../database/database.php';
 $connection = access_database();
 
-$table_name = $_GET("table_name");
+$table_name = $_GET["table_name"];
 
 switch($table_name){
     case 'tasks' :
         $sql = 'SELECT date, subject, name FROM tasks;';
         $result = mysqli_query($connection, $sql);
         if(mysqli_num_rows($result)>0){
-            echo "date,subject,name<br>";
+            echo "date,subject,name\n";
             while($row = $result->fetch_assoc()){
-                echo $row["date"].",".$row["subject"].",".$row["name"]."<br>";
+                echo $row["date"].",".$row["subject"].",".$row["name"]."\n";
             }
         }else{
             header("HHTP/1.0 404 Not Found");
@@ -22,21 +22,21 @@ switch($table_name){
         $sql = 'SELECT subject, name, mark FROM marks WHERE student="' .$_GET['id'].'";';
         $result = mysqli_query($connection, $sql);
         if(mysqli_num_rows($result)>0){
-            echo "subject,name,mark<br>";
+            echo "subject,name,mark\n";
             while($row = $result->fetch_assoc()){
-                echo $row["subject"].",".$row["name"].",".$row["mark"]."<br>";
+                echo $row["subject"].",".$row["name"].",".$row["mark"]."\n";
             }
         } else {
             header("HHTP/1.0 404 Not Found");
         }
         break;
-    case timetables :
+    case 'timetables':
         $sql = 'SELECT day, hour, subject, room FROM timetables;';
         $result = mysqli_query($connection, $sql);
         if(mysqli_num_rows($result)>0){
-            echo "day,hour,subject,room<br>";
+            echo "day,hour,subject,room\n";
             while($row = $result->fetch_assoc()){
-                echo $row["day"].",".$row["hour"].",".$row["subject"].",".$row["room"]."<br>";
+                echo $row["day"].",".$row["hour"].",".$row["subject"].",".$row["room"]."\n";
             }
         }else{
             header("HHTP/1.0 404 Not Found");
