6e26f578be0dff85ef4eeda704fcac64e400476b
Sergi Mus <sergi.mus@gmail.com>
2019-11-20 12:51:48 +0100

    Hotfix

--- client/controller.py
+++ client/controller.py
@@ -4,6 +4,7 @@ from coms.core import CommunicationManager
 
 URL = 'http://10.0.30.203:8000/python/connect_server.php'
 
+
 class Controller:
     def __init__(self, interface):
         self._interface = interface
